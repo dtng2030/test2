a8d55b19ea5332114beb4efa75a3a7e03831bdde
dtng2030 <100981082+dtng2030@users.noreply.github.com>
2022-03-05 10:10:53 +0700

    Add files via upload

---
+++ bai5.cpp
@@ -0,0 +1,11 @@
+//bai5tonggio
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(){
+ int g1,g2,p1,p2,s1,s2;
+ cin>>g1>>p1>>s1;
+ cin>>g2>>p2>>s2;
+ cout<<"cong: "<<g1+g2<<"gio "<<p1+p2<<"phut "<<s1+s2<<"giay"<<endl;
+ cout<<"tru: "<<g1-g2<<" gio"<<p1-p2<<" phut "<<s1-s2<<"giay"<<endl;
+ return 0; }
